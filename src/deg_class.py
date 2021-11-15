10e809ade28720e7b350eeb6fb8bb0e577526c72
grapisar <giacomo.rapi@gmail.com>
2021-11-15 18:06:16 +0100

    added wgcc computation

--- src/deg_class.py
+++ src/deg_class.py
@@ -36,15 +36,15 @@ class AdaptiveDegradation:
     def in_degree_distribution(self,G,kk):
 
         kin_list = list([G.in_degree(n) for n in G.nodes()])
-        pk_in = [kin_list.count(i)/len(kin_list) for i in kk]
-        pk0in = pk_in
+        pk0in = [kin_list.count(i)/len(kin_list) for i in kk]
         
         return pk0in
 
-    def out_degree_distribution(self,G,kk):
+    def out_degree_distribution(self,G):
+        kout_max = max_out_deg(G) + 1
+        kk = np.arange(0,kout_max,1)
         k_list = list([G.out_degree(n) for n in G.nodes()])
-        pk_out = [k_list.count(i)/len(k_list) for i in kk]
-        pk0out = pk_out
+        pk0out = [k_list.count(i)/len(k_list) for i in kk]
         
         return pk0out
 
@@ -85,11 +85,46 @@ class AdaptiveDegradation:
                 bar.next()        
         return PW_noad_T,WW_noad
 
-    def degradation_adaptive_multi(self,yy,G,pwk,kk,pk,ww,pw,TOLLK):
+    def degradation_no_adaptive_WGCC(self,M,yy,G,ww,pw,pk0in,TOLLK):
+        dw = ww[1] - ww[0]
+        G_noad = G.copy()
+        N = nx.number_of_nodes(G)
+        WGCC_noad = []
+        WGCC_noad_T = []
 
+        with Bar('NO ADAPTIVE - MODEL:', max=len(yy)) as bar:
+            t1 = time.time()
+            for y in yy:
+                pkin_noad = PFK_NOAD(pk0in,y,pw,ww,dw,TOLLK)
+                pkout_noad  = pkin_noad
+                WGCC_noad_T.append(fixed_point_directed(np.outer(pkout_noad,pkin_noad)))
+                bar.next()
+            Dt1 = time.time() - t1
+
+        with Bar('NO ADAPTIVE - MC:', max=M) as bar:
+            t2 = time.time()
+            for i in range(M):
+                G_noad2 = G_noad.copy()
+                assign_weights(G_noad2,self.weight_distribution_type)
+                wgcc_n = []
+                for y in yy:
+                    remove_thresh(G_noad2,y,0)
+                    sw_noad = len(max(nx.weakly_connected_components(G_noad2), key=len))/N
+                    wgcc_n.append(sw_noad)
+                WGCC_noad.append(wgcc_n)
+                bar.next()
+            Dt2 = time.time() - t2
+
+        print('Model Time, No Adaptive:',Dt1)
+        print('MC Time, No Adaptive:',Dt2)        
+        
+        return WGCC_noad_T,WGCC_noad
+
+
+    def degradation_adaptive_multi(self,yy,G,pwk,kk,pk,ww,pw,TOLLK):
         dw = ww[1] - ww[0]
         PW_ad_T = []
-        WW_ad = []
+        WW_ad = []        
 
         with Bar('Adaptive',max=len(yy)) as bar:
             for y in yy:
@@ -98,7 +133,7 @@ class AdaptiveDegradation:
                 wpred = pw
 
 
-                pwk_ = PFKEX(wkpred,y,kk,ww,dw,kpred,TOLLK,1,1)
+                pwk_ = PFKEX(wkpred,y,kk,ww,dw,kpred,TOLLK,1)
                     
                 kk = np.arange(0,len(pwk_)+1,1)
 
@@ -130,5 +165,74 @@ class AdaptiveDegradation:
 
                 bar.next()
 
-
         return PW_ad_T,WW_ad
+
+
+    def degradation_adaptive_multi_WGCC(self,M,yy,G,pwk,kk,pk,pk0out,ww,pw,TOLLK):
+        dw = ww[1] - ww[0]
+        WGCC_T = []
+        WGCC = []
+        N = nx.number_of_nodes(G)
+
+        with Bar('ADAPTIVE - MODEL:',max=len(yy)) as bar:
+            t1 = time.time()
+            for y in yy:
+                wkpred = pwk
+                kpred = pk
+                wpred = pw
+
+                pwk_ = PFKEX_APPROX(wkpred,y,kk,ww,dw,kpred,TOLLK,1,12)
+                    
+                kk = np.arange(0,len(pwk_)+1,1)
+
+                pkex = []
+                for k in range(0,len(pwk_)):
+                    pkex.append(integ(pwk_[k],dw))
+                meank = np.sum(pkex)
+
+                pk = [0]*len(kk)
+                for k in range(1,len(kk)): 
+                    pk[k] = pkex[k-1]/k
+                pk[0] = 1 - np.sum(pk)
+
+                pkout = PFK_OUT(kpred,wpred,y,ww,dw,kk)
+            
+                WGCC_T.append(fixed_point_directed(np.outer(pkout,pk)))
+
+                pwk = pwk_/meank
+                pkex = pkex/meank
+
+
+                pw = 0
+                for q in pwk:
+                    pw += q
+
+                bar.next()
+            Dt1 = time.time() - t1
+
+        with Bar('ADAPTIVE - MC:',max=M) as bar:
+            t2 = time.time()
+            for i in range(M):
+                G2 = G.copy()
+                assign_weights(G2,self.weight_distribution_type)
+                wgcc = []
+                for y in yy:
+                    remove_thresh(G2,y,1)
+
+                    if not nx.is_empty(G2):	
+                        sw = len(max(nx.weakly_connected_components(G2), key=len))/N
+                        wgcc.append(sw)
+                    else:
+                        wgcc.append(0)
+                WGCC.append(wgcc)
+                bar.next()
+            Dt2 = time.time() - t2       
+
+
+
+        print('Model Time, Adaptive:',Dt1)
+        print('MC Time, Adaptive:',Dt2)
+
+        return WGCC_T,WGCC
+
+
